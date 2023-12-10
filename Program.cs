f1a8e11e8027197261b65e8d066d3e8ddc23536a
evaldo <evaaldormj@gmail.com>
2023-12-10 13:24:43 -0300

    Feat: Entrega de projeto

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Telefone Iphone:");
+
+Smartphone iphone = new Iphone(numero: "123456", modelo: "Modelo 1", imei: "111111", memoria: 64);
+iphone.Ligar();
+iphone.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Telefone Nokia:");
+
+Smartphone nokia = new Nokia(numero: "23456", modelo: "Modelo 2", imei: "222222", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("TikTok");
